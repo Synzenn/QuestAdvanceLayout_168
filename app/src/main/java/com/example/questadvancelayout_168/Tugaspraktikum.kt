ddf6c6ccae4ca9a68ed41f3c6c507777d3225872
Synzenn <velriel42@gmail.com>
2025-10-25 18:26:37 +0700

    membuat library yang dibutuhkan

---
+++ app/src/main/java/com/example/questadvancelayout_168/Tugaspraktikum.kt
@@ -0,0 +1,29 @@
+package com.example.questadvancelayout_168
+
+import androidx.compose.foundation.Image
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questadvancelayout_168.ui.theme.PrimaryBackground
+import com.example.questadvancelayout_168.ui.theme.MenuCardBg
+import com.example.questadvancelayout_168.ui.theme.IconCircleBg
+import com.example.questadvancelayout_168.ui.theme.TextPrimary
+import com.example.questadvancelayout_168.ui.theme.TextSecondary
+import com.example.questadvancelayout_168.ui.theme.CopyrightText
+import com.example.questadvancelayout_168.ui.theme.SocialIconBg
+
+
